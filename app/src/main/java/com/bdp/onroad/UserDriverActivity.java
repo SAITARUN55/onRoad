19ba4de93cf6c0e3fb5372ffda2887424ec32ef5
PrajjwalS <prajjwals12th@gmail.com>
2019-04-02 23:31:00 +0530

    Added maps Dependencies

--- app/src/main/java/com/bdp/onroad/UserDriverActivity.java
+++ app/src/main/java/com/bdp/onroad/UserDriverActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -16,21 +17,32 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.bdp.onroad.LoginActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-
-public class UserDriverActivity extends AppCompatActivity
+public class UserDriverActivity extends FragmentActivity implements OnMapReadyCallback
 {
 
-
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_driver);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapp);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+            
+
+
+    }
+
 }
